b20c8b78bff030f809f4881d08b9ea1e812094ba
Tis Lais <tistististis@gmail.com>
2021-07-26 16:38:49 -0700

    Add react skeleton and basic display of colors

---
+++ src/components/app/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ColorGenerator from '../color-generator/ColorGenerator';
+
+export default function App() {
+  return <ColorGenerator />;
+}
---
+++ src/components/color-generator/ColorGenerator.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class ColorGenerator extends Component {
+  state = {
+    selectedColor: 'red'
+  }
+
+  handleColorChange = color => {
+    this.setState({ selectedColor: color });
+  }
+
+  render() {
+    const colors = [
+      'red',
+      'orange', 
+      'yellow', 
+      'green', 
+      'blue', 
+      'indigo', 
+      'violet'
+    ];
+    return (
+      <>
+        {colors.map((color) => (
+          <div key={color}>{color}</div>
+        ))}
+      </>
+    );
+  }
+}
